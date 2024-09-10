8cbfd28ef8fd62901cf91b225e8b2edb1dedcb2b
headring <myhakiki@gmail.com>
2024-09-10 23:27:29 +0900

    devtools render의미 파악

--- apps/render-playground/app/page.tsx
+++ apps/render-playground/app/page.tsx
@@ -1,7 +1,45 @@
+'use client'
+import { memo, useState } from "react";
+
+const InnerComponent = (props) => {
+  const {count} = props
+  if(count){
+    return <div>has count InnerComponent{count} counting</div>
+  }
+  return <div>inner InnerComponent</div>
+}
+
+const MemoComponent1 = memo(() => {
+  console.log('no props')
+  return (<div>non memo</div>)
+})
+
+const MemoComponent2 = memo((props: {memoCount: number}) => {
+  console.log('here')
+  return (<div>counting memo: {props.memoCount}</div>)
+})
 
 
-export default function Home() {
+function Home(props) {
+  const [count, setCount] = useState(0)
+  const [memoCount, setMemoCount] = useState(0)
+  
   return (
-    <div>render-playground</div>
+    <div>
+      <div>render-playground - devtools render check</div>
+      <ul>devtools의 render는 Render Phase를 의미. 자신의 상태가 변경되거나, props가 변경되거나, 부모가 변경되어서 하위값들이 render phase 진입</ul>
+      <div>count: {count}</div>
+      <div>memoCount: {memoCount}</div>
+      <div onClick={() => setMemoCount(prev => prev+ 1)}>memo updater</div>
+    <div onClick={() => setCount(prev => prev + 1)}>click</div>
+    <div><InnerComponent count={count} /></div>
+    <MemoComponent1  />
+    <MemoComponent2 memoCount={memoCount}  />
+    <div>{props.children}</div>
+    </div>
   );
+}
+export default function PageHome() {
+  const [hoeme, setHome] = useState('home')
+ return (<Home>여기는 children {hoeme}  <InnerComponent/>  </Home>)
 }
