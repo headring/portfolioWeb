ff7eaec1a571e77d8846912dc36f7ad3ea2ae714
SangHyun Kim <myhakiki@gmail.com>
2024-09-12 23:08:23 +0900

    chore: eslint 적용 및 prettier extenstion 활성화 (#2)
    * chore: eslint 적용 및 prettier extenstion 활성화
    
    * chore: render-playground eslint 설정commit 누락추가
    
    * chore: render-playground tsconfig 설정

--- apps/render-playground/app/page.tsx
+++ apps/render-playground/app/page.tsx
@@ -1,45 +1,53 @@
-'use client'
+"use client";
 import { memo, useState } from "react";
 
 const InnerComponent = (props) => {
-  const {count} = props
-  if(count){
-    return <div>has count InnerComponent{count} counting</div>
+  const { count } = props;
+  if (count) {
+    return <div>has count InnerComponent{count} counting</div>;
   }
-  return <div>inner InnerComponent</div>
-}
+  return <div>inner InnerComponent</div>;
+};
 
 const MemoComponent1 = memo(() => {
-  console.log('no props')
-  return (<div>non memo</div>)
-})
+  console.log("no props");
+  return <div>non memo</div>;
+});
 
-const MemoComponent2 = memo((props: {memoCount: number}) => {
-  console.log('here')
-  return (<div>counting memo: {props.memoCount}</div>)
-})
-
+const MemoComponent2 = memo((props: { memoCount: number }) => {
+  console.log("here");
+  return <div>counting memo: {props.memoCount}</div>;
+});
 
 function Home(props) {
-  const [count, setCount] = useState(0)
-  const [memoCount, setMemoCount] = useState(0)
-  
+  const [count, setCount] = useState(0);
+  const [memoCount, setMemoCount] = useState(0);
+
   return (
     <div>
       <div>render-playground - devtools render check</div>
-      <ul>devtools의 render는 Render Phase를 의미. 자신의 상태가 변경되거나, props가 변경되거나, 부모가 변경되어서 하위값들이 render phase 진입</ul>
+      <ul>
+        devtools의 render는 Render Phase를 의미. 자신의 상태가 변경되거나,
+        props가 변경되거나, 부모가 변경되어서 하위값들이 render phase 진입
+      </ul>
       <div>count: {count}</div>
       <div>memoCount: {memoCount}</div>
-      <div onClick={() => setMemoCount(prev => prev+ 1)}>memo updater</div>
-    <div onClick={() => setCount(prev => prev + 1)}>click</div>
-    <div><InnerComponent count={count} /></div>
-    <MemoComponent1  />
-    <MemoComponent2 memoCount={memoCount}  />
-    <div>{props.children}</div>
+      <div onClick={() => setMemoCount((prev) => prev + 1)}>memo updater</div>
+      <div onClick={() => setCount((prev) => prev + 1)}>click</div>
+      <div>
+        <InnerComponent count={count} />
+      </div>
+      <MemoComponent1 />
+      <MemoComponent2 memoCount={memoCount} />
+      <div>{props.children}</div>
     </div>
   );
 }
 export default function PageHome() {
-  const [hoeme, setHome] = useState('home')
- return (<Home>여기는 children {hoeme}  <InnerComponent/>  </Home>)
+  const [hoeme, setHome] = useState("home");
+  return (
+    <Home>
+      여기는 children {hoeme} <InnerComponent />{" "}
+    </Home>
+  );
 }
